7373d43d0a69646cff0cfd765da4376bc2565ca5
Alexander <alexanderfjor@gmail.com>
2024-06-12 19:29:27 +0300

    Small changes + pickle files
    Small changes + pickle files

--- src/train.py
+++ src/train.py
@@ -27,25 +27,43 @@ def train():
 
     train_data, test_data, train_label, test_label = train_test_split(X, y, test_size=0.2, random_state=0)
 
-    # lowercase = False, защото вече сме премахнали стоп думите и превърнали текста в малки букви
-    # ngram_range = (1, 2) , защото искаме да ползваме униграми и биграми
+    '''
+    lowercase = False, защото вече сме премахнали стоп думите и превърнали текста в малки букви
+    ngram_range = (1, 2) , защото искаме да ползваме униграми и биграми
+    '''
     vectorizer = TfidfVectorizer(max_features=50000, ngram_range=(1, 2), lowercase=False)
     vec_train_data = vectorizer.fit_transform(train_data)
     vec_train_data = vec_train_data.toarray()
     vec_test_data = vectorizer.transform(test_data).toarray()
-    oversampler = RandomUnderSampler(sampling_strategy=1)
+
+    '''
+    Ще обучи предоставения класификатор чрез по-малка извадка от потока от дадени наблюдения, така че разпределението на класа, 
+    видяно от класификатора, следва дадено желано разпределение
+    '''
+    oversampler = RandomUnderSampler(sampling_strategy=0.5)
     vec_train_data, train_label = oversampler.fit_resample(vec_train_data, train_label)
     training_data = pd.DataFrame(vec_train_data , columns=vectorizer.get_feature_names_out())
     testing_data = pd.DataFrame(vec_test_data , columns= vectorizer.get_feature_names_out())
+    
+    '''
+    Задаване на повече итерации, за да се увеличи точността на модела
+    Това ще работи добре с повече данни
+    '''
     clf = LogisticRegression(max_iter=1000, verbose=2)
     clf.fit(training_data, train_label)
     y_pred = clf.predict(testing_data)
+
     print(classification_report(test_label , y_pred))
+    
+    '''
+    Запазване на модела и векторизатора, за да може да се използва за предсказване на нови данни
+    '''
     joblib.dump(clf, '../data/model.pkl')
     joblib.dump(vectorizer, '../data/vectorizer.pkl')
+
     return accuracy_score(test_label, y_pred)
 
 check_class_distribution(FILEPATH)
-accuracy = train(FILEPATH)
 
+accuracy = train()
 print(accuracy)
